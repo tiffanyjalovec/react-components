e7fafced2ba3f4f1bdba3c2cd1f17a468394c0c4
Tiffany Jalovec <carr.tiffany@gmail.com>
2019-12-30 16:50:32 -0700

    so excited I got my list to work!

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 // TODO
+var GroceryList = (props) => (
+  <div>
+  <ul>
+    {props.groceries.map((grocery, i) =>
+    <GroceryListItem key ={i} groceries = {grocery}/>
+    )}
+  </ul>
+  </div>
+);
+
+class GroceryListItem extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hover: false
+    }
+  };
+
+  onListItemHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
+  }
+
+
+render() {
+  var style = {
+    fontWeight: this.state.hover ? 'bold' : 'normal'
+  }
+  return (
+    <li style = {style} onMouseOver = {this.onListItemHover.bind(this)}>{this.props.groceries}</li>
+  );
+ }
+
+};
+
+
+
+ReactDOM.render(<GroceryList groceries={['Pickles', 'Kale']} />, document.getElementById("app"));
